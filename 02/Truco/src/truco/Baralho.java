682d6bb68574b845863797714b69a7fc9a4c89c4
Pedro Paulo Valadares Brum <pedropaulovaladaresbrum@Pedros-MacBook-Pro.local>
2017-05-09 15:48:03 -0300

    pedro primeiro commit

--- 02/Truco/src/truco/Baralho.java
+++ 02/Truco/src/truco/Baralho.java
@@ -10,5 +10,88 @@ package truco;
  * @author rafael
  */
 public class Baralho {
-    
+
+	private List<Carta> baralho;
+
+	public Baralho() throws Exception {
+
+		this.baralho = new ArrayList<Carta>();
+
+		baralho.add(new Carta("♠", "🂡", 14));
+		baralho.add(new Carta("♠", "🂢", 11));
+		baralho.add(new Carta("♠", "🂣", 12));
+		baralho.add(new Carta("♠", "🂤", 0));
+		baralho.add(new Carta("♠", "🂥", 1));
+		baralho.add(new Carta("♠", "🂦", 2));
+		baralho.add(new Carta("♠", "🂧", 3));
+		baralho.add(new Carta("♠", "🂨", 4));
+		baralho.add(new Carta("♠", "🂩", 5));
+		baralho.add(new Carta("♠", "🂪", 6));
+		baralho.add(new Carta("♠", "🂫", 7));
+		baralho.add(new Carta("♠", "🂬", 8));
+		baralho.add(new Carta("♠", "🂭", 9));
+		baralho.add(new Carta("♠", "🂮", 10));
+
+
+		baralho.add(new Carta("♥", "🂱", 10));
+		baralho.add(new Carta("♥", "🂲", 11));
+		baralho.add(new Carta("♥", "🂳", 12));
+		baralho.add(new Carta("♥", "🂴", 0));
+		baralho.add(new Carta("♥", "🂵", 1));
+		baralho.add(new Carta("♥", "🂶", 2));
+		baralho.add(new Carta("♥", "🂷", 15));
+		baralho.add(new Carta("♥", "🂸", 3));
+		baralho.add(new Carta("♥", "🂹", 4));
+		baralho.add(new Carta("♥", "🂺", 5));
+		baralho.add(new Carta("♥", "🂻", 6));
+		baralho.add(new Carta("♥", "🂼", 7));
+		baralho.add(new Carta("♥", "🂽", 8));
+		baralho.add(new Carta("♥", "🂾", 9));
+
+
+		baralho.add(new Carta("♦", "🃁", 10));
+		baralho.add(new Carta("♦", "🃂", 11));
+		baralho.add(new Carta("♦", "🃃", 12));
+		baralho.add(new Carta("♦", "🃄", 0));
+		baralho.add(new Carta("♦", "🃅", 1));
+		baralho.add(new Carta("♦", "🃆", 2));
+		baralho.add(new Carta("♦", "🃇", 13));
+		baralho.add(new Carta("♦", "🃈", 3));
+		baralho.add(new Carta("♦", "🃉", 4));
+		baralho.add(new Carta("♦", "🃊", 5));
+		baralho.add(new Carta("♦", "🃋", 6));
+		baralho.add(new Carta("♦", "🃌", 7));
+		baralho.add(new Carta("♦", "🃍", 8));
+		baralho.add(new Carta("♦", "🃎", 9));
+
+
+		baralho.add(new Carta("♣", "🃑", 10));
+		baralho.add(new Carta("♣", "🃒", 11));
+		baralho.add(new Carta("♣", "🃓", 12));
+		baralho.add(new Carta("♣", "🃔", 16));
+		baralho.add(new Carta("♣", "🃕", 0));
+		baralho.add(new Carta("♣", "🃖", 1));
+		baralho.add(new Carta("♣", "🃗", 2));
+		baralho.add(new Carta("♣", "🃘", 3));
+		baralho.add(new Carta("♣", "🃙", 4));
+		baralho.add(new Carta("♣", "🃚", 5));
+		baralho.add(new Carta("♣", "🃛", 6));
+		baralho.add(new Carta("♣", "🃜", 7));
+		baralho.add(new Carta("♣", "🃝", 8));
+		baralho.add(new Carta("♣", "🃞", 9));
+	}
+
+	public void embaralha() {
+
+		Collections.shuffle(baralho);
+	}
+
+	public void corta() {
+
+
+	}
+
+	public void distribuiCartas() {
+
+	}
 }
