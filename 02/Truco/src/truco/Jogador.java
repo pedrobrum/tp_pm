5140190f45ebc75131873c58cefd59cb69fd767b
Rafael Grandsire <rafael.grandsire@gmail.com>
2017-05-09 16:52:54 -0300

    R

--- 02/Truco/src/truco/Jogador.java
+++ 02/Truco/src/truco/Jogador.java
@@ -1,13 +1,14 @@
 package truco;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
 public class Jogador {
     private Carta carta[];
-    private final JLabel labelCarta[];
-    private final JLabel labelJogada;
+    private final JButton labelCarta[];
+    private final JButton labelJogada;
     
-    public Jogador(JLabel labelCarta[], JLabel labelJogada){
+    public Jogador(JButton labelCarta[], JButton labelJogada){
         this.labelCarta = labelCarta;
         this.labelJogada = labelJogada;
         for (int i = 0; i < 3; i++)
--- 02/Truco/src/truco/Truco.java
+++ 02/Truco/src/truco/Truco.java
@@ -1,6 +1,6 @@
 package truco;
 
-import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class Truco extends javax.swing.JFrame {
 
@@ -10,10 +10,10 @@ public class Truco extends javax.swing.JFrame {
     public Truco() {
         initComponents();
         jogador = new Jogador[4];
-        jogador[0] = new Jogador(new JLabel[]{labelJ0C0, labelJ0C1, labelJ0C2}, labelJ0C);
-        jogador[1] = new Jogador(new JLabel[]{labelJ1C0, labelJ1C1, labelJ1C2}, labelJ1C);
-        jogador[2] = new Jogador(new JLabel[]{labelJ2C0, labelJ2C1, labelJ2C2}, labelJ2C);
-        jogador[3] = new Jogador(new JLabel[]{labelJ3C0, labelJ3C1, labelJ3C2}, labelJ3C);
+        jogador[0] = new Jogador(new JButton[]{buttonJ0C0, buttonJ0C1, buttonJ0C2}, buttonJ0C);
+//        jogador[1] = new Jogador(new JLabel[]{labelJ1C0, labelJ1C1, labelJ1C2}, labelJ1C);
+//        jogador[2] = new Jogador(new JLabel[]{labelJ2C0, labelJ2C1, labelJ2C2}, labelJ2C);
+//        jogador[3] = new Jogador(new JLabel[]{labelJ3C0, labelJ3C1, labelJ3C2}, labelJ3C);
     }
 
     @SuppressWarnings("unchecked")
@@ -28,9 +28,6 @@ public class Truco extends javax.swing.JFrame {
         labelTituloTime2 = new javax.swing.JLabel();
         labelTime1Ponto = new javax.swing.JLabel();
         labelTime2Ponto = new javax.swing.JLabel();
-        labelJ0C0 = new javax.swing.JLabel();
-        labelJ0C1 = new javax.swing.JLabel();
-        labelJ0C2 = new javax.swing.JLabel();
         labelJ1C0 = new javax.swing.JLabel();
         labelJ1C1 = new javax.swing.JLabel();
         labelJ1C2 = new javax.swing.JLabel();
@@ -40,10 +37,10 @@ public class Truco extends javax.swing.JFrame {
         labelJ3C0 = new javax.swing.JLabel();
         labelJ3C1 = new javax.swing.JLabel();
         labelJ3C2 = new javax.swing.JLabel();
-        labelJ0C = new javax.swing.JLabel();
-        labelJ1C = new javax.swing.JLabel();
-        labelJ2C = new javax.swing.JLabel();
-        labelJ3C = new javax.swing.JLabel();
+        buttonJ0C = new javax.swing.JButton();
+        buttonJ0C0 = new javax.swing.JButton();
+        buttonJ0C1 = new javax.swing.JButton();
+        buttonJ0C2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -108,15 +105,6 @@ public class Truco extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        labelJ0C0.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
-        labelJ0C0.setText("🂡\t");
-
-        labelJ0C1.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
-        labelJ0C1.setText("🂡\t");
-
-        labelJ0C2.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
-        labelJ0C2.setText("🂡\t");
-
         labelJ1C0.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
         labelJ1C0.setText("🂡\t");
 
@@ -144,17 +132,44 @@ public class Truco extends javax.swing.JFrame {
         labelJ3C2.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
         labelJ3C2.setText("🂡\t");
 
-        labelJ0C.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
-        labelJ0C.setText("🂡\t");
+        buttonJ0C.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
+        buttonJ0C.setText("🂡\t");
+        buttonJ0C.setBorder(null);
+        buttonJ0C.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonJ0CActionPerformed(evt);
+            }
+        });
 
-        labelJ1C.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
-        labelJ1C.setText("🂡\t");
+        buttonJ0C0.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
+        buttonJ0C0.setText("?");
+        buttonJ0C0.setBorder(null);
+        buttonJ0C0.setPreferredSize(new java.awt.Dimension(49, 57));
+        buttonJ0C0.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonJ0C0ActionPerformed(evt);
+            }
+        });
 
-        labelJ2C.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
-        labelJ2C.setText("🂡\t");
+        buttonJ0C1.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
+        buttonJ0C1.setText("?");
+        buttonJ0C1.setBorder(null);
+        buttonJ0C1.setPreferredSize(new java.awt.Dimension(49, 57));
+        buttonJ0C1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonJ0C1ActionPerformed(evt);
+            }
+        });
 
-        labelJ3C.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
-        labelJ3C.setText("🂡\t");
+        buttonJ0C2.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
+        buttonJ0C2.setText("?");
+        buttonJ0C2.setBorder(null);
+        buttonJ0C2.setPreferredSize(new java.awt.Dimension(49, 57));
+        buttonJ0C2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonJ0C2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout panelBaseLayout = new javax.swing.GroupLayout(panelBase);
         panelBase.setLayout(panelBaseLayout);
@@ -177,7 +192,7 @@ public class Truco extends javax.swing.JFrame {
                         .addGroup(panelBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(panelBaseLayout.createSequentialGroup()
                                 .addComponent(panelPontuacao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 77, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 75, Short.MAX_VALUE)
                                 .addComponent(labelJ2C0)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(labelJ2C1)
@@ -185,21 +200,15 @@ public class Truco extends javax.swing.JFrame {
                                 .addComponent(labelJ2C2))
                             .addGroup(panelBaseLayout.createSequentialGroup()
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGroup(panelBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addGroup(panelBaseLayout.createSequentialGroup()
-                                        .addComponent(labelJ1C)
-                                        .addGap(55, 55, 55))
-                                    .addGroup(panelBaseLayout.createSequentialGroup()
-                                        .addComponent(labelJ0C0)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(labelJ0C1))
-                                    .addComponent(labelJ0C)
-                                    .addComponent(labelJ2C))
+                                .addComponent(buttonJ0C0, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(panelBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(labelJ0C2)
-                                    .addComponent(labelJ3C))))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 73, Short.MAX_VALUE)
+                                    .addComponent(buttonJ0C)
+                                    .addGroup(panelBaseLayout.createSequentialGroup()
+                                        .addComponent(buttonJ0C1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(buttonJ0C2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 75, Short.MAX_VALUE)
                         .addComponent(buttonIniciar)))
                 .addContainerGap())
         );
@@ -217,31 +226,26 @@ public class Truco extends javax.swing.JFrame {
                 .addGap(72, 72, 72)
                 .addGroup(panelBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(panelBaseLayout.createSequentialGroup()
-                        .addGroup(panelBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(labelJ3C0)
-                            .addComponent(labelJ2C))
+                        .addComponent(labelJ3C0)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(panelBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(labelJ3C1)
-                            .addComponent(labelJ1C)
-                            .addComponent(labelJ3C))
+                        .addComponent(labelJ3C1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(panelBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(labelJ3C2)
-                            .addComponent(labelJ0C))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(buttonIniciar))
-                    .addGroup(panelBaseLayout.createSequentialGroup()
+                        .addComponent(labelJ3C2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 69, Short.MAX_VALUE)
+                        .addGroup(panelBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(buttonIniciar, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(buttonJ0C0, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(buttonJ0C1, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(buttonJ0C2, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, panelBaseLayout.createSequentialGroup()
                         .addComponent(labelJ1C0)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(labelJ1C1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(labelJ1C2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 69, Short.MAX_VALUE)
+                        .addGap(0, 0, 0)
                         .addGroup(panelBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(labelJ0C0)
-                            .addComponent(labelJ0C1)
-                            .addComponent(labelJ0C2))
+                            .addComponent(labelJ1C2)
+                            .addComponent(buttonJ0C, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addContainerGap())))
         );
 
@@ -251,14 +255,11 @@ public class Truco extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(panelBase, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, 0))
+                .addComponent(panelBase, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(panelBase, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(panelBase, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -266,9 +267,25 @@ public class Truco extends javax.swing.JFrame {
 
     private void buttonIniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonIniciarActionPerformed
         Baralho b = new Baralho();
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 1; i++)
             jogador[i].receberCarta(new Carta[]{b.getTopo(), b.getTopo(), b.getTopo()});
     }//GEN-LAST:event_buttonIniciarActionPerformed
+
+    private void buttonJ0C0ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonJ0C0ActionPerformed
+            jogador[0].jogarCarta(0);
+    }//GEN-LAST:event_buttonJ0C0ActionPerformed
+
+    private void buttonJ0CActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonJ0CActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonJ0CActionPerformed
+
+    private void buttonJ0C1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonJ0C1ActionPerformed
+        jogador[0].jogarCarta(1);
+    }//GEN-LAST:event_buttonJ0C1ActionPerformed
+
+    private void buttonJ0C2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonJ0C2ActionPerformed
+        jogador[0].jogarCarta(2);
+    }//GEN-LAST:event_buttonJ0C2ActionPerformed
     
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -280,21 +297,16 @@ public class Truco extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buttonIniciar;
-    private javax.swing.JLabel labelJ0C;
-    private javax.swing.JLabel labelJ0C0;
-    private javax.swing.JLabel labelJ0C1;
-    private javax.swing.JLabel labelJ0C2;
-    private javax.swing.JLabel labelJ1C;
+    private javax.swing.JButton buttonJ0C;
+    private javax.swing.JButton buttonJ0C0;
+    private javax.swing.JButton buttonJ0C1;
+    private javax.swing.JButton buttonJ0C2;
     private javax.swing.JLabel labelJ1C0;
     private javax.swing.JLabel labelJ1C1;
     private javax.swing.JLabel labelJ1C2;
-    private javax.swing.JLabel labelJ1C2N1;
-    private javax.swing.JLabel labelJ1C2S1;
-    private javax.swing.JLabel labelJ2C;
     private javax.swing.JLabel labelJ2C0;
     private javax.swing.JLabel labelJ2C1;
     private javax.swing.JLabel labelJ2C2;
-    private javax.swing.JLabel labelJ3C;
     private javax.swing.JLabel labelJ3C0;
     private javax.swing.JLabel labelJ3C1;
     private javax.swing.JLabel labelJ3C2;
@@ -304,7 +316,6 @@ public class Truco extends javax.swing.JFrame {
     private javax.swing.JLabel labelTituloTime1;
     private javax.swing.JLabel labelTituloTime2;
     private javax.swing.JPanel panelBase;
-    private javax.swing.JPanel panelJ1C4;
     private javax.swing.JPanel panelPontuacao;
     // End of variables declaration//GEN-END:variables
 }
