66ae649e809ae0531957652128f42470c1415786
Rafael Grandsire <rafael.grandsire@gmail.com>
2017-05-09 16:21:15 -0300

    R

--- 02/Truco/src/truco/Baralho.java
+++ 02/Truco/src/truco/Baralho.java
@@ -1,97 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package truco;
 
-/**
- *
- * @author rafael
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Baralho {
 
-	private List<Carta> baralho;
+    private final List<Carta> baralho;
+    private final Random random;
 
-	public Baralho() throws Exception {
+    public Baralho() {
+        this.baralho = new ArrayList<>();
 
-		this.baralho = new ArrayList<Carta>();
-
-		baralho.add(new Carta("♠", "🂡", 14));
-		baralho.add(new Carta("♠", "🂢", 11));
-		baralho.add(new Carta("♠", "🂣", 12));
-		baralho.add(new Carta("♠", "🂤", 0));
-		baralho.add(new Carta("♠", "🂥", 1));
-		baralho.add(new Carta("♠", "🂦", 2));
-		baralho.add(new Carta("♠", "🂧", 3));
-		baralho.add(new Carta("♠", "🂨", 4));
-		baralho.add(new Carta("♠", "🂩", 5));
-		baralho.add(new Carta("♠", "🂪", 6));
-		baralho.add(new Carta("♠", "🂫", 7));
-		baralho.add(new Carta("♠", "🂬", 8));
-		baralho.add(new Carta("♠", "🂭", 9));
-		baralho.add(new Carta("♠", "🂮", 10));
+        baralho.add(new Carta("🂡", 14));
+        baralho.add(new Carta("🂢", 11));
+        baralho.add(new Carta("🂣", 12));
+        baralho.add(new Carta("🂤", 0));
+        baralho.add(new Carta("🂥", 1));
+        baralho.add(new Carta("🂦", 2));
+        baralho.add(new Carta("🂧", 3));
+        baralho.add(new Carta("🂪", 6));
+        baralho.add(new Carta("🂫", 7));
+        baralho.add(new Carta("🂬", 8));
+        baralho.add(new Carta("🂭", 9));
+        baralho.add(new Carta("🂮", 10));
 
 
-		baralho.add(new Carta("♥", "🂱", 10));
-		baralho.add(new Carta("♥", "🂲", 11));
-		baralho.add(new Carta("♥", "🂳", 12));
-		baralho.add(new Carta("♥", "🂴", 0));
-		baralho.add(new Carta("♥", "🂵", 1));
-		baralho.add(new Carta("♥", "🂶", 2));
-		baralho.add(new Carta("♥", "🂷", 15));
-		baralho.add(new Carta("♥", "🂸", 3));
-		baralho.add(new Carta("♥", "🂹", 4));
-		baralho.add(new Carta("♥", "🂺", 5));
-		baralho.add(new Carta("♥", "🂻", 6));
-		baralho.add(new Carta("♥", "🂼", 7));
-		baralho.add(new Carta("♥", "🂽", 8));
-		baralho.add(new Carta("♥", "🂾", 9));
+        baralho.add(new Carta("🂱", 10));
+        baralho.add(new Carta("🂲", 11));
+        baralho.add(new Carta("🂳", 12));
+        baralho.add(new Carta("🂴", 0));
+        baralho.add(new Carta("🂵", 1));
+        baralho.add(new Carta("🂶", 2));
+        baralho.add(new Carta("🂷", 15));
+        baralho.add(new Carta("🂺", 5));
+        baralho.add(new Carta("🂻", 6));
+        baralho.add(new Carta("🂼", 7));
+        baralho.add(new Carta("🂽", 8));
+        baralho.add(new Carta("🂾", 9));
 
 
-		baralho.add(new Carta("♦", "🃁", 10));
-		baralho.add(new Carta("♦", "🃂", 11));
-		baralho.add(new Carta("♦", "🃃", 12));
-		baralho.add(new Carta("♦", "🃄", 0));
-		baralho.add(new Carta("♦", "🃅", 1));
-		baralho.add(new Carta("♦", "🃆", 2));
-		baralho.add(new Carta("♦", "🃇", 13));
-		baralho.add(new Carta("♦", "🃈", 3));
-		baralho.add(new Carta("♦", "🃉", 4));
-		baralho.add(new Carta("♦", "🃊", 5));
-		baralho.add(new Carta("♦", "🃋", 6));
-		baralho.add(new Carta("♦", "🃌", 7));
-		baralho.add(new Carta("♦", "🃍", 8));
-		baralho.add(new Carta("♦", "🃎", 9));
+        baralho.add(new Carta("🃁", 10));
+        baralho.add(new Carta("🃂", 11));
+        baralho.add(new Carta("🃃", 12));
+        baralho.add(new Carta("🃄", 0));
+        baralho.add(new Carta("🃅", 1));
+        baralho.add(new Carta("🃆", 2));
+        baralho.add(new Carta("🃇", 13));
+        baralho.add(new Carta("🃊", 5));
+        baralho.add(new Carta("🃋", 6));
+        baralho.add(new Carta("🃌", 7));
+        baralho.add(new Carta("🃍", 8));
+        baralho.add(new Carta("🃎", 9));
 
 
-		baralho.add(new Carta("♣", "🃑", 10));
-		baralho.add(new Carta("♣", "🃒", 11));
-		baralho.add(new Carta("♣", "🃓", 12));
-		baralho.add(new Carta("♣", "🃔", 16));
-		baralho.add(new Carta("♣", "🃕", 0));
-		baralho.add(new Carta("♣", "🃖", 1));
-		baralho.add(new Carta("♣", "🃗", 2));
-		baralho.add(new Carta("♣", "🃘", 3));
-		baralho.add(new Carta("♣", "🃙", 4));
-		baralho.add(new Carta("♣", "🃚", 5));
-		baralho.add(new Carta("♣", "🃛", 6));
-		baralho.add(new Carta("♣", "🃜", 7));
-		baralho.add(new Carta("♣", "🃝", 8));
-		baralho.add(new Carta("♣", "🃞", 9));
-	}
+        baralho.add(new Carta("🃑", 10));
+        baralho.add(new Carta("🃒", 11));
+        baralho.add(new Carta("🃓", 12));
+        baralho.add(new Carta("🃔", 16));
+        baralho.add(new Carta("🃕", 0));
+        baralho.add(new Carta("🃖", 1));
+        baralho.add(new Carta("🃗", 2));
+        baralho.add(new Carta("🃚", 5));
+        baralho.add(new Carta("🃛", 6));
+        baralho.add(new Carta("🃜", 7));
+        baralho.add(new Carta("🃝", 8));
+        baralho.add(new Carta("🃞", 9));
+        Collections.shuffle(baralho);
+        random = new Random();
+    }
 
-	public void embaralha() {
-
-		Collections.shuffle(baralho);
-	}
-
-	public void corta() {
-
-
-	}
-
-	public void distribuiCartas() {
-
-	}
+    public Carta getTopo() {
+        int i = random.nextInt(baralho.size());
+        Carta c = baralho.get(i);
+        baralho.remove(i);
+        return c;
+    }
 }
--- 02/Truco/src/truco/Jogador.java
+++ 02/Truco/src/truco/Jogador.java
@@ -5,15 +5,30 @@ import javax.swing.JLabel;
 public class Jogador {
     private Carta carta[];
     private final JLabel labelCarta[];
+    private final JLabel labelJogada;
     
-    public Jogador(JLabel labelCarta[]){
+    public Jogador(JLabel labelCarta[], JLabel labelJogada){
         this.labelCarta = labelCarta;
+        this.labelJogada = labelJogada;
+        for (int i = 0; i < 3; i++)
+                labelCarta[i].setText("");
+        labelJogada.setText("");
     }
     
     public void receberCarta(Carta carta[]){
         this.carta = carta;
         for (int i = 0; i < 3; i++)
             labelCarta[i].setText(carta[i].getSimbolo());
+        labelJogada.setText("");
     }
     
+    public boolean jogarCarta(int i){
+        if (carta[i] != null){
+            labelCarta[i].setText("");
+            labelJogada.setText(carta[i].getSimbolo());
+            carta[i] = null;
+            return true;
+        }
+        return false;
+    }
 }
